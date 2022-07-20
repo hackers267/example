aab8b952d48a886db1968121e69f8eb69fac895e
silence_zhpf <silence_zhpf@aliyun.com>
2022-07-20 10:12:49 +0800

    feat: ✨ Color添加更多方法示例
    silence_zhpf<silence_zhpf@aliyun.com>

--- src/color.rs
+++ src/color.rs
@@ -1,27 +1,36 @@
-use std::fmt::{Display, Formatter, Write};
+use std::fmt::{Display, Formatter};
 
 #[cfg(test)]
 mod test {
     use super::*;
     #[test]
-    fn color_test(){
+    fn hi_red_on_yellow() {
         let hi = "Hello".red().on_yellow();
-        assert_eq!(format!("{}",hi),"\x1B[31;43mHello\x1B[0m");
+        assert_eq!(format!("{}", hi), "\x1B[31;43mHello\x1B[0m");
+    }
+    #[test]
+    fn hi_yellow_on_red() {
+        let hi = "Hello".yellow().on_red();
+        assert_eq!(format!("{}", hi), "\x1B[33;41mHello\x1B[0m");
     }
 }
 
 trait Colorize {
-    const FG_RED:&'static str="31";
-    const BG_YELLOW:&'static str="43";
-    fn red(self)->ColorString;
-    fn on_yellow(self)->ColorString;
+    const FG_RED: &'static str = "31";
+    const FG_YELLOW: &'static str = "33";
+    const BG_RED: &'static str = "41";
+    const BG_YELLOW: &'static str = "43";
+    fn red(self) -> ColorString;
+    fn yellow(self) -> ColorString;
+    fn on_yellow(self) -> ColorString;
+    fn on_red(self) -> ColorString;
 }
 
-#[derive(Default,Eq, PartialEq)]
+#[derive(Default, Eq, PartialEq)]
 struct ColorString {
     input: String,
-    fgcolor:String,
-    bgcolor:String,
+    fgcolor: String,
+    bgcolor: String,
 }
 
 impl Display for ColorString {
@@ -44,34 +53,64 @@ impl Display for ColorString {
     }
 }
 
-impl Colorize for &str{
+impl Colorize for &str {
     fn red(self) -> ColorString {
-        ColorString{
-            input:self.to_string(),
-            fgcolor:ColorString::FG_RED.to_string(),
+        ColorString {
+            input: self.to_string(),
+            fgcolor: ColorString::FG_RED.to_string(),
+            ..ColorString::default()
+        }
+    }
+
+    fn yellow(self) -> ColorString {
+        ColorString {
+            input: self.to_string(),
+            fgcolor: ColorString::FG_YELLOW.to_string(),
             ..ColorString::default()
         }
     }
 
     fn on_yellow(self) -> ColorString {
         ColorString {
-            input:self.to_string(),
-            bgcolor:ColorString::BG_YELLOW.to_string(),
+            input: self.to_string(),
+            bgcolor: ColorString::BG_YELLOW.to_string(),
+            ..ColorString::default()
+        }
+    }
+
+    fn on_red(self) -> ColorString {
+        ColorString {
+            input: self.to_string(),
+            bgcolor: ColorString::BG_RED.to_string(),
             ..ColorString::default()
         }
     }
 }
-impl Colorize for ColorString{
+impl Colorize for ColorString {
     fn red(self) -> Self {
         Self {
-            fgcolor:ColorString::FG_RED.to_string(),
+            fgcolor: ColorString::FG_RED.to_string(),
+            ..self
+        }
+    }
+
+    fn yellow(self) -> Self {
+        Self {
+            fgcolor: ColorString::FG_YELLOW.to_string(),
             ..self
         }
     }
 
     fn on_yellow(self) -> Self {
         Self {
-            bgcolor:ColorString::BG_YELLOW.to_string(),
+            bgcolor: ColorString::BG_YELLOW.to_string(),
+            ..self
+        }
+    }
+
+    fn on_red(self) -> Self {
+        Self {
+            bgcolor: ColorString::BG_RED.to_string(),
             ..self
         }
     }
